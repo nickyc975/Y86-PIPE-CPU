9a70da50803a8e95b2b064c41d65038b71f239eb
Nicky <chenjinlong2016@gmail.com>
2017-12-22 13:26:14 +0800

    working on execute part.

---
+++ y86cpu.srcs/sources_1/new/execute.v
@@ -0,0 +1,16 @@
+`include "define.v"
+
+module execute(
+    input wire [`ICODE_BUS] E_icode,
+    input wire [`IFUN_BUS] E_ifun,
+    input wire [`DATA_BUS] E_valC,
+    input wire [`DATA_BUS] E_valA,
+    input wire [`DATA_BUS] E_valB,
+    input wire [`REG_ADDR_BUS] E_dstE,
+    
+    output reg e_Cnd,
+    output reg [`DATA_BUS] e_valA,
+    output reg [`REG_ADDR_BUS] e_dstE,
+    output reg [`DATA_BUS] e_valE
+    );
+endmodule
