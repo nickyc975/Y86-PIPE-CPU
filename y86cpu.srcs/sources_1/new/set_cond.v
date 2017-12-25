35c9b2bb00d073a4ac2075873bea9b6c941ac33d
Nicky <chenjinlong2016@gmail.com>
2017-12-25 21:34:14 +0800

    CXX instructions work now.

--- y86cpu.srcs/sources_1/new/set_cond.v
+++ y86cpu.srcs/sources_1/new/set_cond.v
@@ -25,35 +25,31 @@ module set_cond(
         OF = 1'B0;
     end
     
-    always @(posedge clk)
-        begin
-            if(rst == `RST_EN)
-            begin
-                ZF <= 1'B0;
-                SF <= 1'B0;
-                OF <= 1'B0;
-            end
-            else if(W_stat == `AOK && m_stat == `AOK && 
-                   (E_icode == `OPQ || (E_icode == `IXX && E_ifun != `IRMOVQ)))
-            begin
-                ZF <= ZF_i;
-                SF <= SF_i;
-                OF <= OF_i;
-            end
-        end
-    
     always @(*)
     begin
-        if(E_icode ==  `JXX || E_icode ==  `CXX)
+        if(rst != `RST_EN /* && W_stat == `AOK && m_stat == `AOK */ &&
+          (E_icode == `OPQ || (E_icode == `IXX && E_ifun != `IRMOVQ)))
+            begin
+                ZF = ZF_i;
+                SF = SF_i;
+                OF = OF_i;
+            end
+        else
+            begin
+                ZF = ZF;
+                SF = SF;
+                OF = OF;
+            end
+        if(E_icode == `JXX || E_icode == `CXX)
             begin
                 case(E_ifun)
-                    `JLE:      e_Cnd = ZF || ((!SF && OF) || SF);
+                    `JLE:      e_Cnd = ZF || (!SF && OF) || SF;
                      `JL:      e_Cnd = (!SF && OF) || SF;
                      `JE:      e_Cnd = ZF;
                     `JNE:      e_Cnd = !ZF;
-                    `JGE:      e_Cnd = ZF || ((SF && OF) || !SF);
+                    `JGE:      e_Cnd = ZF || (SF && OF) || !SF;
                      `JG:      e_Cnd = (SF && OF) || !SF;
-                     default:  e_Cnd = 1'B0;
+                     default:  e_Cnd = 1'B1;
                 endcase
                     
                 if(e_Cnd == 1'B1)
